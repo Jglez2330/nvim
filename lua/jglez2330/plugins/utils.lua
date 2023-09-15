8ba27d39976a660c18aa6d2a613f611e63c8cbe2
Joseph González <josegonzalez2330@gmail.com>
2023-09-14 23:04:33 -0600

    Add telescope search type

--- lua/jglez2330/plugins/utils.lua
+++ lua/jglez2330/plugins/utils.lua
@@ -8,27 +8,27 @@ return {
                 require("telescope").load_extension("fzf")
             end,
         }, },
-        -- opts = function()
-        --     return {
-        --         defaults = {
-        --             path_display = { "truncate" },
-        --             sorting_strategy = "ascending",
-        --             layout_config = {
-        --                 horizontal = {
-        --                     prompt_position = "top",
-        --                     preview_width = 0.55,
-        --                 },
-        --                 vertical = {
-        --                     mirror = false,
-        --                 },
-        --                 width = 0.87,
-        --                 height = 0.80,
-        --                 preview_cutoff = 120,
-        --             },
-        --
-        --         },
-        --     }
-        -- end,
+        opts = function()
+            return {
+                defaults = {
+                    path_display = { "truncate" },
+                    sorting_strategy = "ascending",
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "top",
+                            preview_width = 0.55,
+                        },
+                        vertical = {
+                            mirror = false,
+                        },
+                        width = 0.87,
+                        height = 0.80,
+                        preview_cutoff = 120,
+                    },
+
+                },
+            }
+        end,
     },
     {
         "williamboman/mason.nvim",
