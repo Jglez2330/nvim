b393ed4c829a85acea686182305166892c44cb39
Joseph González <josegonzalez2330@gmail.com>
2023-10-19 22:17:55 -0600

    add transparent bg

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -38,48 +38,48 @@ end)
 
 
 -- -- Only define once
--- if not require'lspconfig.configs'.hdl_checker then
---   require'lspconfig.configs'.hdl_checker = {
---     default_config = {
---     cmd = {"hdl_checker", "--lsp", };
---     filetypes = {"vhdl", "verilog", "systemverilog"};
---       root_dir = function(fname)
---         -- will look for the .hdl_checker.config file in parent directory, a
---         -- .git directory, or else use the current directory, in that order.
---         local util = require'lspconfig'.util
---         return util.root_pattern('.hdl_checker.config')(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
---       end;
---       settings = {};
---     };
---   }
--- end
---
--- require'lspconfig'.hdl_checker.setup{}
---
-
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
--- Only define once
-local lspconfig = require 'lspconfig'
-local configs = require 'lspconfig.configs'
-
--- Manual add rust_hdl server
-if not configs.rust_hdl then
-  configs.rust_hdl = {
+if not require'lspconfig.configs'.hdl_checker then
+  require'lspconfig.configs'.hdl_checker = {
     default_config = {
-      cmd = {'vhdl_ls'};
-      filetypes = { "vhdl" };
+    cmd = {"hdl_checker", "--lsp", };
+    filetypes = {"vhdl", "verilog", "systemverilog"};
       root_dir = function(fname)
-        return lspconfig.util.root_pattern('vhdl_ls.toml')(fname)
+        -- will look for the .hdl_checker.config file in parent directory, a
+        -- .git directory, or else use the current directory, in that order.
+        local util = require'lspconfig'.util
+        return util.root_pattern('.hdl_checker.config.json')(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
       end;
       settings = {};
     };
   }
 end
 
-require 'lspconfig'.rust_hdl.setup{}
+require'lspconfig'.hdl_checker.setup{}
 
+
+-- (Optional) Configure lua language server for neovim
+-- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+--
+-- -- Only define once
+-- local lspconfig = require 'lspconfig'
+-- local configs = require 'lspconfig.configs'
+--
+-- -- Manual add rust_hdl server
+-- if not configs.rust_hdl then
+--   configs.rust_hdl = {
+--     default_config = {
+--       cmd = {'vhdl_ls'};
+--       filetypes = { "vhdl" };
+--       root_dir = function(fname)
+--         return lspconfig.util.root_pattern('vhdl_ls.toml')(fname)
+--       end;
+--       settings = {};
+--     };
+--   }
+-- end
+--
+-- require 'lspconfig'.rust_hdl.setup{}
+--
 -- return 'lspconfig'.rust_hdl.setup{}
 
 lsp.setup()
