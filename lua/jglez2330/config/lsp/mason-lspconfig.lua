239d94c8bc3ffb03952e1f21faf57e1eb33cc2a8
Joseph Gonzalez <joseph.gonzalez@joby.aero>
2025-01-24 12:11:27 -0600

    Return to previous working config

--- lua/jglez2330/config/lsp/mason-lspconfig.lua
+++ lua/jglez2330/config/lsp/mason-lspconfig.lua
@@ -6,9 +6,9 @@ if not mlspconfig_ok then
     jglez2330.load_plugin_error("mason-lspconfig")
     return
 end
-
-
 local util = require 'lspconfig.util'
+
+
 
 -- =============================================
 -- ========== Configuration
@@ -31,47 +31,16 @@ local handlers = {
             root_dir = util.find_git_ancestor,
         }
     end,
-    ["hdl_checker"] = function()
+    ["hdl_checker"] = function ()
         require 'lspconfig'.hdl_checker.setup {
-            cmd = { "hdl_checker", "--lsp" },
-            filetypes = { "vhdl" },
-            root_dir = util.root_pattern('.hdl_checker.config'),
-        }
-    end,
-    ["svlangserver"] = function()
-        require 'lspconfig'.svlangserver.setup {
-            default_config = {
-                cmd = { 'svlangserver' },
-                filetypes = { 'verilog', 'systemverilog' },
-                root_dir = function(fname)
-                    return util.root_pattern '.svlangserver' (fname)
-                        or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-                end,
-                single_file_support = true,
-                settings = {
-                    systemverilog = {
-                        includeIndexing = { '*.{v,vh,sv,svh}', '**/*.{v,vh,sv,svh}', '~/Documents/uvm-1.2/**/*.{v,vh,sv,svh}' },
-                    },
-                },
-            },
-            docs = {
-                description = [[
-                                https://github.com/imc-trading/svlangserver
-
-                                Language server for SystemVerilog.
-
-                                `svlangserver` can be installed via `npm`:
-
-                                ```sh
-                                $ npm install -g @imc-trading/svlangserver
-                                ```
-                                ]],
-            },
+            cmd = { "hdl_checker", "--lsp", },
+            filetypes = { "vhdl", "verilog", "systemverilog" },
+            root_dir = util.root_pattern('.hdl_checker.config') or util.find_git_ancestor(fname) or util.path.dirname(fname),
         }
     end,
 }
 local config = {
-    ensure_installed = { "lua_ls", "vhdl_ls", "verible", },
+    ensure_installed = { "lua_ls", "hdl_checker", "vhdl_ls", "verible", },
     --automatic_installation = false,
     handlers = handlers,
 }
