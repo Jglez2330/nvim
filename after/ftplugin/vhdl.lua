7a793304727ae7be03c1ea96f64e69467b5e39c9
Joseph González <josegonzalez2330@gmail.com>
2025-11-07 01:52:02 -0600

    add file config

---
+++ after/ftplugin/vhdl.lua
@@ -0,0 +1,5 @@
+-- VHDL indentation
+vim.bo.expandtab = true      -- use spaces
+vim.bo.tabstop = 3           -- width of Tab char
+vim.bo.shiftwidth = 3        -- autoindent width
+vim.bo.softtabstop = 3       -- backspace feels right
