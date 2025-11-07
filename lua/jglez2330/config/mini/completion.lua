875cdb055248495c9cab449476fb43e28a17d079
Joseph González <josegonzalez2330@gmail.com>
2025-11-06 20:13:40 -0600

    Test border

--- lua/jglez2330/config/mini/completion.lua
+++ lua/jglez2330/config/mini/completion.lua
@@ -21,7 +21,7 @@ local config = {
 	-- - `height` and `width` are maximum dimensions.
 	-- - `border` defines border (as in `nvim_open_win()`).
 	window = {
-		info = { height = 25, width = 80, border = "none" },
+		info = { height = 25, width = 80, border = "single" },
 		signature = { height = 25, width = 80, border = "single" },
 	},
 
